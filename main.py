d90e901892d6844db204694d575045f5d7898f6b
SurPetrosian <surach2020@gmail.com>
2024-08-24 23:24:33 +0300

    работа с API

--- main.py
+++ main.py
@@ -114,3 +114,59 @@
 #     if user_input == guide["pk"]:
 #         print(guide["fields"]["full_name"], 'туров:', guide["fields"]["tours_count"])
 #           print(guide["fields"]["bio"])
+
+from datetime import datetime
+import requests
+import json
+import os
+
+
+API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
+CURRENCY_RATES_FILE = 'currency_rates.json'
+
+def main():
+    while True:
+        currency = input('Введите название валюты (USD или EUR): ')
+        if currency not in ("USD", "EUR"):
+            print('Некорректный ввод')
+            continue
+
+        rate = get_currency_rate(currency)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        print(f'{currency} к рублю: {rate}')
+        data = {'currency': currency, 'rate': rate, 'timestamp': timestamp}
+        save_to_json(data)
+
+        choice = input('Выберите действие(1 - продолжить, 2 - выйти)')
+        if choice == "1":
+            continue
+        elif choice == "2":
+            break
+        else:
+            print ('Некорректный ввод')
+
+def get_currency_rate(base: str) -> float:
+    """Получает курс от API и возвращает в виде float"""
+
+    url = "https://api.apilayer.com/exchangerates_data/latest"
+    response = requests.get(url, headers={'apikey': API_KEY}, params={'base': base})
+    rate = response.json()['rates']['RUB']
+    return rate
+
+
+def save_to_json(data: dict) -> None:
+    """Сохраняет данные в json файл """
+    with open(CURRENCY_RATES_FILE, 'a') as f:
+        if os.stat(CURRENCY_RATES_FILE).st_size == 0:
+            json.dump([data], f)
+        else:
+            with open(CURRENCY_RATES_FILE) as json_file:
+                data_list = json.load(json_file)
+            data_list.append(data)
+            with open(CURRENCY_RATES_FILE, "w") as json_file:
+                json.dump(data_list, json_file)
+
+
+if __name__ == '__main__':
+    main()
